d929537dff706cfdd10002bcb3f67ec58e2358ab
Glitch (ms-filemetadata) <none>
2018-05-21 14:06:35 +0000

    🚩🤗 Checkpoint ./public/client.js:1118247/238 ./server.js:1118247/91

--- public/client.js
+++ public/client.js
@@ -1,34 +1,21 @@
 function upload(){
-  return new Promise(function(resolve,reject){
-  
   var upFile = document.getElementById("up-file");
-    
     $.ajax({
     type:"POST",
     url:"/get-file-size",
     success: function(response){
       if("files" in upFile){
         if(upFile.files.length == 0){
-          reject("No file selected");
+          return "No File Selected";
         }
       else{
-        resolve(upFile);
+        console.log(upFile);
+        return upFile;
       }  
       }
     },
     error: function(err){
-    reject(err);
+    return err;
     }  
     })
-      
-    
-    
-  
-    
-    
-    
-    
-    
-    
-  });
 }
--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.post("/get-file-size",function(request,response){
+  console.log(request.body);
+
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
