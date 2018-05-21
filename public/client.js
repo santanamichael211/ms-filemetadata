881071a701ab8fec81492a5ac55aa249bcf8ed57
Glitch (ms-filemetadata) <none>
2018-05-21 14:10:19 +0000

    🛣✨ Checkpoint ./views/index.html:1118247/186 ./public/client.js:1118247/156

--- public/client.js
+++ public/client.js
@@ -1,20 +1,23 @@
 function upload(){
+  
   var upFile = document.getElementById("up-file");
     $.ajax({
     type:"POST",
     url:"/get-file-size",
     success: function(response){
+      
       if("files" in upFile){
         if(upFile.files.length == 0){
           return "No File Selected";
         }
       else{
-        console.log(upFile);
+        
         return upFile;
       }  
       }
     },
     error: function(err){
+      console.log("Hello");
     return err;
     }  
     })
